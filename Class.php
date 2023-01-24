2267042e71444ebe8cf136ff7d95767527b60c2b
rodhisz <rodhisz.45@gmail.com>
2023-01-24 11:29:37 +0700

    2

--- Class.php
+++ Class.php
@@ -17,19 +17,41 @@ class Mobil2
 class Mobil3
 {
     public $roda;
+    public $pintu;
+
     public function jalan()
     {
+        $pintu = 4; //variable lokal didalam OOP
         echo 'Mobil berjalan';
     }
 }
 
 $avanza = new Mobil3();
+// $avanza->roda = 4;
 
-$avanza->roda = 4;
+// $hino = new Mobil3();
+// $hino->roda=10;
+
+// echo $hino->jalan();
+// echo $hino->roda;
+// echo PHP_EOL;
 
 echo $avanza->jalan();
 echo PHP_EOL;
 echo $avanza->roda;
 echo PHP_EOL;
+
+class Koneksi
+{
+    public function connect($username, $password, 
+    $host = 'localhost', $port = 3306)
+    {
+        echo "$username, $password, $host, $port";
+    }
+}
+
+$database = new Koneksi();
+$database->connect();
+
 
 ?>
